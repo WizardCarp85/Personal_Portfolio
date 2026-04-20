15af65584ba399ea64186fb5866dce08d517facd
WizardCarp85 <yashkumarnimje85@gmail.com>
2026-04-21 01:05:17 +0530

    working on login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import './App.css'
+import Login from "./components/Login";
+import { useAuth } from "./store/auth";
+import { AGENTS,ClippyProvider } from "@react95/clippy";
 
 function App() {
+  const authenticated = useAuth((state)=>state.authenticated);
 
   return (
-    <>
-    <div>
-      
+    <div style={{width:"100%",background:"#098684",minHeight:"100vh",position:"relative"}}>
+      <img src="/logo.png" width={400} style={{position:"absolute",top:"50%",left:"50%",transform:"translate(-50%,-50%)"}}/>
+      {/* login page if the user is unauthenticated */}
+      {!authenticated && <Login/>}
+
+      {/* main page authenticated */}
+      {
+        authenticated && (
+          <ClippyProvider agentName={AGENTS.MERLIN}>
+            <div className="fixed">
+              {/* Desktop icons */}
+            </div>
+          </ClippyProvider>
+        )
+      }
+
     </div>
-    </>
   )
 }
 
---
+++ src/store/auth.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface AuthState {
+    authenticated: boolean;
+    login:()=>void;
+    logout:()=>void;
+}
+
+export const useAuth=create<AuthState>((set)=>({
+    authenticated:false,
+    login:()=>set({authenticated:true}),
+    logout:()=>set({authenticated:false})
+}))
